9b142598d8657bc5eaa155103d1332ff67a3b254
JoaoMafra <jcmafraneto@gmail.com>
2026-05-08 20:31:28 -0300

    feat: adicionando models da inscricao

--- inscricoes/models.py
+++ inscricoes/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+
+class Inscricao(models.Model):
+
+    DISPONIBILIDADE = (
+        ('manha', 'Manhã'),
+        ('tarde', 'Tarde'),
+        ('noite', 'Noite'),
+    )
+
+    nome = models.CharField(
+        max_length=100
+    )
+
+    email = models.EmailField(
+        unique=True
+    )
+
+    telefone = models.CharField(
+        max_length=20
+    )
+
+    disponibilidade = models.CharField(
+        max_length=20,
+        choices=DISPONIBILIDADE
+    )
+
+    observacoes = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    ativa = models.BooleanField(
+        default=True
+    )
+
+    data_criacao = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.nome
