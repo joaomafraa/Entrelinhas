bd622fb672add37f3f822f78f2e44e9f88608297
JoaoMafra <jcmafraneto@gmail.com>
2026-05-08 21:37:38 -0300

    feat: colocando inscricao no admin do projeto

--- inscricoes/admin.py
+++ inscricoes/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Inscricao
+
+
+@admin.register(Inscricao)
+class InscricaoAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'nome',
+        'email',
+        'disponibilidade',
+        'ativa',
+        'data_criacao',
+    )
+
+    search_fields = (
+        'nome',
+        'email',
+    )
+
+    list_filter = (
+        'disponibilidade',
+        'ativa',
+        'data_criacao',
+    )
